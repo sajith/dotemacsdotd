30f2f084705baa15b0c567968134c585b1173c92
Sajith Sasidharan <sajith@hcoop.net>
2020-09-17 16:12:34 -0400

    Add projectile

--- init.el
+++ init.el
@@ -757,6 +757,13 @@
 
 ;; ---------------------------------------------------------------------
 
+(use-package projectile
+  :config
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
+;; ---------------------------------------------------------------------
+
 (use-package server
   :init (server-start))
 
