49893545509cac6b65cbdeca4cc2c67818b952e7
Sajith Sasidharan <sajith@hcoop.net>
2021-05-08 10:34:47 -0400

    Add ivy

--- init.el
+++ init.el
@@ -176,6 +176,62 @@
 
 ;; ---------------------------------------------------------------------
 
+(use-package ivy
+  :ensure t
+  :config
+  (ivy-mode 1)
+  (setq ivy-use-virtual-buffers nil)
+  (setq ivy-count-format "(%d/%d) ")
+  ;; (setq enable-recursive-minibuffers nil)
+  :bind
+  (:map ivy-mode-map
+        ("C-'" . ivy-avy)))
+
+(use-package counsel
+  :ensure t
+  :bind
+  (
+   ;; Ivy-based interface to standard commands.
+   ("C-s"      . swiper-isearch)
+   ("M-x"      . counsel-M-x)
+   ("C-x C-f"  . counsel-find-file)
+   ("M-y"      . counsel-yank-pop)
+   ("<f1> f"   . counsel-describe-function)
+   ("<f1> v"   . counsel-describe-variable)
+   ("<f1> l"   . counsel-find-library)
+   ("<f2> i"   . counsel-info-lookup-symbol)
+   ("<f2> u"   . counsel-unicode-char)
+   ("<f2> j"   . counsel-set-variable)
+   ("C-x b"    . ivy-switch-buffer)
+   ("C-c v"    . ivy-push-view)
+   ("C-c V"    . ivy-pop-view)
+
+   ;; Ivy-based interface to shell and system tools.
+   ("C-c c"    . counsel-compile)
+   ("C-c g"    . counsel-git)
+   ("C-c j"    . counsel-git-grep)
+   ("C-c L"    . counsel-git-log)
+   ("C-c k"    . counsel-rg)
+   ("C-c m"    . counsel-linux-app)
+   ("C-c n"    . counsel-fzf)
+   ("C-x l"    . counsel-locate)
+   ("C-c J"    . counsel-file-jump)
+   ("C-S-o"    . counsel-rhythmbox)
+   ("C-c w"    . counsel-wmctrl)
+
+   ;; Ivy-resume and other commands.  Ivy-resume resumes the last
+   ;; Ivy-based completion.
+   ("C-c C-r"  . ivy-resume)
+   ("C-c b"    . counsel-bookmark)
+   ("C-c d"    . counsel-descbinds)
+   ("C-c g"    . counsel-git)
+   ("C-c o"    . counsel-outline)
+   ("C-c t"    . counsel-load-theme)
+   ("C-c F"    . counsel-org-file)
+   ))
+
+;; ---------------------------------------------------------------------
+
 ;; ;; Some more shaved Yak hair, for eventual use
 
 ;; (defun my-load (filename)
@@ -316,7 +372,8 @@
 
 ;; ---------------------------------------------------------------------
 
-(setq-default show-trailing-whitespace t)
+;; (setq-default show-trailing-whitespace t)
+(setq-default show-trailing-whitespace nil)
 
 ;; (add-hook 'before-save-hook 'delete-trailing-whitespace)
 ;; (add-hook 'before-save-hook 'whitespace-cleanup)
