b11413404febfd504c41efc454f585a21e62e583
Sajith Sasidharan <sajith@hcoop.net>
2026-05-21 10:05:24 -0500

    Add org-roam

--- init.el
+++ init.el
@@ -863,6 +863,23 @@
 
 ;; ---------------------------------------------------------------------
 
+(use-package org-roam
+  :init
+  (setq org-roam-v2-ack t)
+  :custom
+  (org-roam-directory (file-truename "~/org/roam"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-db-autosync-mode))
+
+;; ---------------------------------------------------------------------
+
 ;; http://jblevins.org/projects/deft/
 (use-package deft
   :init
