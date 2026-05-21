0249ca6261a9156332845f03744734f82774763f
Sajith Sasidharan <sajith@hcoop.net>
2026-05-21 11:13:55 -0500

    Remove org-mobile

--- init.el
+++ init.el
@@ -846,16 +846,6 @@
 
 ;; ---------------------------------------------------------------------
 
-(setq
- ;; Set to the location of your Org files on your local system
- org-directory "~/org"
- ;; Set to the name of the file where new notes will be stored
- org-mobile-inbox-for-pull "~/org/flagged.org"
- ;; Set to <your Dropbox root directory>/MobileOrg.
- org-mobile-directory "~/Dropbox/MobileOrg")
-
-;; ---------------------------------------------------------------------
-
 ;; for js-comit run-js command.
 ;; TODO: the bindings don't run correctly; check.
 (use-package js2-mode
